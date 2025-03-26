179e552a17aaa9a10cd1deefabb6cdb8a5fe7aa8
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-03-26 17:27:18 +0000

    edited html

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<!-- ICS2O-Unit2-01-PHP -->
+<!-- ICS2O-Unit2-01-HTML-MDL -->
 <html lang="en-ca">
 
 <head>
@@ -30,49 +30,48 @@
     sizes="16x16"
     href="./favicon-16x16.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>The "Hello, World!" program, PHP</title>
+  <title>The 'Hello, World' program, PHP</title>
 </head>
 
-
-<body>
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
-      <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">The "Hello, World!" program, PHP</span>
-      </div>
-    </header>
-    <main class="mdl-layout__content">
-      <div class="right-image">
-        <img src="./images/Hello_World_Brian_Kernighan_1978.jpg" alt="laptop image" />
-      </div>
-      <div class="page-content">Instructions here ...</div>
-      <br />
-      <!-- Simple Textfield for integers-->
-      <form action="answer.php" method="GET">
-        <div class="mdl-textfield mdl-js-textfield">
-          <!-- input pattern attribute -->
-          <input
-            class="mdl-textfield__input"
-            type="text"
-            pattern="-?[0-9]*(\.[0-9]+)?"
-            id="length" />
-          <!-- mdl-textfield__label -->
-          <label class="mdl-textfield__label" for="demo-input">Length of ... (mm)</label>
-          <!-- class "mdl-textfield__error" -->
-          <span class="mdl-textfield__error">Input is not a number</span>
+  <body>
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">The 'Hello, World' program, PHP</span>
         </div>
+      </header>
+      <main class="mdl-layout__content">
+        <div class="right-image">
+          <img src="./images/Hello_World_Brian_Kernighan_1978.jpg" alt="laptop image" />
+        </div>
+        <div class="page-content">Instructions here ...</div>
         <br />
-        <!-- Accent-colored raised button with ripple -->
-        <button
-          class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-          type="submit">
-          Calculate
-        </button>
-      </form>
-      <br />
-    </main>
-  </div>
-</body>
+        <!-- Simple Textfield for integers-->
+        <form action="answer.php" method="GET">
+          <div class="mdl-textfield mdl-js-textfield">
+            <!-- input pattern attribute -->
+            <input
+              class="mdl-textfield__input"
+              type="text"
+              pattern="-?[0-9]*(\.[0-9]+)?"
+              id="length" />
+            <!-- mdl-textfield__label -->
+            <label class="mdl-textfield__label" for="demo-input">Length of ... (mm)</label>
+            <!-- class "mdl-textfield__error" -->
+            <span class="mdl-textfield__error">Input is not a number</span>
+          </div>
+          <br />
+          <!-- Accent-colored raised button with ripple -->
+          <button
+            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+            type="submit">
+            Calculate
+          </button>
+        </form>
+        <br />
+      </main>
+    </div>
+  </body>
 
 </html>
