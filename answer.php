d5cfccb36f0e6c329be32d6523f1452a71495fd2
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-04-30 14:09:25 +0000

    edit php

--- answer.php
+++ answer.php
@@ -33,22 +33,21 @@
   <title>The 'Hello, World!' program, PHP</title>
 </head>
 
-
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">The 'Hello, World! program, PHP</span>
+        <span class="mdl-layout-title">The "Hello, World!" program, PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/notebook-animation.svg" alt="laptop image" />
+        <img src="./images/image.png" />
       </div>
-      <div class="page-content">
+      <div class="page content">
         <?php
-        echo "Hello, World!";
+        echo "Hello World!";
         ?>
       </div>
       <br />
